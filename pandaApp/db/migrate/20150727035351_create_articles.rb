2dd0c4230ff719457872fd2399dc7d4723015aa5
Kush <kushthaker@gmail.com>
2015-07-27 00:02:17 -0400

    article model updated

--- pandaApp/db/migrate/20150727035351_create_articles.rb
+++ pandaApp/db/migrate/20150727035351_create_articles.rb
@@ -1,6 +1,7 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
+      t.string :article_type
       t.string :content
       t.string :url
       t.references :user, index: true, foreign_key: true
--- pandaApp/db/schema.rb
+++ pandaApp/db/schema.rb
@@ -11,26 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724172342) do
+ActiveRecord::Schema.define(version: 20150727035351) do
 
   create_table "articles", force: :cascade do |t|
+    t.string   "article_type"
     t.string   "content"
     t.string   "url"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
-
-  create_table "profiles", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",                            null: false
